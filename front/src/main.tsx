6697518182d3069625ed45a30fcd20c91b59ad0e
Mayo <66140252+mayo56@users.noreply.github.com>
2024-02-05 07:31:35 +0000

    update front

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* Route principale */}
+        <Route path='/' />
+
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
